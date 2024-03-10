f9db9b2d3d0c0b48fe24970c4d0566136ad918b0
Ehud Gordon <ehud.gordon@campus.technion.ac.il>
2024-03-10 21:52:19 +0200

    uncommented fragment

--- include/Fragment.h
+++ include/Fragment.h
@@ -1,43 +1,43 @@
-//// Fragment.h
-//#pragma once
-//#include "Light.h"
-//
-//class Fragment {
-//public:
-//	Material material;
-//	vec3 v1;
-//	vec3 v2;
-//	vec3 v3;
-//	vec3 v_center;
-//	vec3 v1_n;
-//	vec3 v2_n;
-//	vec3 v3_n;
-//	vec3 face_normal;
-//	float barycentric_w0;
-//	float barycentric_w1;
-//	float barycentric_w2;
-//
-//	Fragment();
-//
-//	Fragment(const Material& material, 
-//		const vec3& v1, 
-//		const vec3& v2, 
-//		const vec3& v3, 
-//		const vec3& v1_n, 
-//		const vec3& v2_n, 
-//		const vec3& v3_n,
-//		const vec3& face_normal,
-//		float barycentric_w0, 
-//		float barycentric_w1, 
-//		float barycentric_w2)
-//		: material(material), v1(v1), v2(v2), v3(v3), v1_n(v1_n), v2_n(v2_n), 
-//			v3_n(v3_n), face_normal(face_normal), barycentric_w0(barycentric_w0), 
-//			barycentric_w1(barycentric_w1), barycentric_w2(barycentric_w2)
-//	{
-//		this->v_center = (v1 + v2 + v3) / 3;
-//	}
-//
-//	vec3 ComputeColorFlat(std::vector<Light>);
-//	vec3 ComputeColorGouraud(std::vector<Light>);
-//	vec3 ComputeColorPhong(std::vector<Light>);
-//};
+// Fragment.h
+#pragma once
+#include "Light.h"
+
+class Fragment {
+public:
+	Material material;
+	vec3 v1;
+	vec3 v2;
+	vec3 v3;
+	vec3 v_center;
+	vec3 v1_n;
+	vec3 v2_n;
+	vec3 v3_n;
+	vec3 face_normal;
+	float barycentric_w0;
+	float barycentric_w1;
+	float barycentric_w2;
+
+	Fragment();
+
+	Fragment(const Material& material, 
+		const vec3& v1, 
+		const vec3& v2, 
+		const vec3& v3, 
+		const vec3& v1_n, 
+		const vec3& v2_n, 
+		const vec3& v3_n,
+		const vec3& face_normal,
+		float barycentric_w0, 
+		float barycentric_w1, 
+		float barycentric_w2)
+		: material(material), v1(v1), v2(v2), v3(v3), v1_n(v1_n), v2_n(v2_n), 
+			v3_n(v3_n), face_normal(face_normal), barycentric_w0(barycentric_w0), 
+			barycentric_w1(barycentric_w1), barycentric_w2(barycentric_w2)
+	{
+		this->v_center = (v1 + v2 + v3) / 3;
+	}
+
+	vec3 ComputeColorFlat(std::vector<Light>);
+	vec3 ComputeColorGouraud(std::vector<Light>);
+	vec3 ComputeColorPhong(std::vector<Light>);
+};
--- src/Fragment.cpp
+++ src/Fragment.cpp
@@ -1,62 +1,62 @@
-//#include "Fragment.h"
-//
-//Fragment::Fragment()
-//{
-//	this->barycentric_w0 = 0;
-//	this->barycentric_w1 = 0;
-//	this->barycentric_w2 = 0;
-//}
-//
-//vec3 Fragment::ComputeColorFlat(std::vector<Light> lights)
-//{
-//	vec3 color;
-//	vec3 color_diffuse;
-//	vec3 color_specular;
-//	float intensity_sum = 0;
-//
-//	for (auto &light : lights)
-//	{
-//		float current_intensity = light.getLightIntensityAt(Vector3f(this->v_center));
-//		vec3 c1 = this->material.getColorFor(this->v1);
-//		vec3 c2 = this->material.getColorFor(this->v2);
-//		vec3 c3 = this->material.getColorFor(this->v3);
-//		vec3 l = light.getLightDirectionAt(this->v_center);
-//		Vector3f n = this->face_normal.normalized();
-//
-//		if (intensity_sum < MAX_INTENSITY)
-//		{
-//			intensity_sum += current_intensity;
-//		}
-//
-//		if (intensity_sum < MAX_INTENSITY)
-//		{
-//			intensity_sum = MAX_INTENSITY;
-//		}
-//	}
-//
-//	return color;
-//}
-//
-//vec3 Fragment::ComputeColorGouraud(std::vector<Light> lights)
-//{
-//	vec3 color;
-//
-//	for (auto& light : lights)
-//	{
-//		// TODO:
-//	}
-//
-//	return color;
-//}
-//
-//vec3 Fragment::ComputeColorPhong(std::vector<Light> lights)
-//{
-//	vec3 color;
-//
-//	for (auto& light : lights)
-//	{
-//		// TODO:
-//	}
-//
-//	return color;
-//}
+#include "Fragment.h"
+
+Fragment::Fragment()
+{
+	this->barycentric_w0 = 0;
+	this->barycentric_w1 = 0;
+	this->barycentric_w2 = 0;
+}
+
+vec3 Fragment::ComputeColorFlat(std::vector<Light> lights)
+{
+	vec3 color;
+	vec3 color_diffuse;
+	vec3 color_specular;
+	float intensity_sum = 0;
+
+	for (auto &light : lights)
+	{
+		float current_intensity = light.getLightIntensityAt(Vector3f(this->v_center));
+		vec3 c1 = this->material.getColorFor(this->v1);
+		vec3 c2 = this->material.getColorFor(this->v2);
+		vec3 c3 = this->material.getColorFor(this->v3);
+		vec3 l = light.getLightDirectionAt(this->v_center);
+		Vector3f n = this->face_normal.normalized();
+
+		if (intensity_sum < MAX_INTENSITY)
+		{
+			intensity_sum += current_intensity;
+		}
+
+		if (intensity_sum < MAX_INTENSITY)
+		{
+			intensity_sum = MAX_INTENSITY;
+		}
+	}
+
+	return color;
+}
+
+vec3 Fragment::ComputeColorGouraud(std::vector<Light> lights)
+{
+	vec3 color;
+
+	for (auto& light : lights)
+	{
+		// TODO:
+	}
+
+	return color;
+}
+
+vec3 Fragment::ComputeColorPhong(std::vector<Light> lights)
+{
+	vec3 color;
+
+	for (auto& light : lights)
+	{
+		// TODO:
+	}
+
+	return color;
+}
