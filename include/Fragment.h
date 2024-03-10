7c7ba9a6d1b340901a4c239f5ed88c0e56c8317b
Amit Inbar <amitir22@gmail.com>
2024-03-10 07:01:16 +0200

    Started to implement Fragment class

--- include/Fragment.h
+++ include/Fragment.h
@@ -8,6 +8,7 @@ public:
 	Vector3f v1;
 	Vector3f v2;
 	Vector3f v3;
+	Vector3f v_center;
 	Vector3f v1_n;
 	Vector3f v2_n;
 	Vector3f v3_n;
@@ -28,7 +29,9 @@ public:
 		float barycentric_w1, 
 		float barycentric_w2)
 		: material(material), v1(v1), v2(v2), v3(v3), v1_n(v1_n), v2_n(v2_n), v3_n(v3_n), barycentric_w0(barycentric_w0), barycentric_w1(barycentric_w1), barycentric_w2(barycentric_w2)
-	{}
+	{
+		this->v_center = (v1 + v2 + v3) / 3;
+	}
 
 	Vector3f computeColorFlat(std::vector<Light>);
 	Vector3f computeColorGourard(std::vector<Light>);
--- src/Fragment.cpp
+++ src/Fragment.cpp
@@ -7,17 +7,50 @@ Fragment::Fragment()
 	this->barycentric_w2 = 0;
 }
 
-Vector3f Fragment::computeColorFlat(std::vector<Light>)
+Vector3f Fragment::computeColorFlat(std::vector<Light> lights)
 {
-	return Vector3f();
+	Vector3f color;
+	float intensity_sum = 0;
+
+	for (auto &light : lights)
+	{
+		float current_intensity = light.getLightIntensityAt(Vector3f(this->v_center));
+		Vector3f c1 = this->material.getColorFor(this->v1);
+		Vector3f c2 = this->material.getColorFor(this->v2);
+		Vector3f c3 = this->material.getColorFor(this->v3);
+
+		if (intensity_sum < MAX_INTENSITY)
+		{
+			intensity_sum += current_intensity;
+		}
+
+		if (intensity_sum < MAX_INTENSITY)
+		{
+			intensity_sum = MAX_INTENSITY;
+		}
+	}
 }
 
-Vector3f Fragment::computeColorGourard(std::vector<Light>)
+Vector3f Fragment::computeColorGourard(std::vector<Light> lights)
 {
-	return Vector3f();
+	Vector3f color;
+
+	for (auto& light : lights)
+	{
+		// TODO:
+	}
+
+	return color;
 }
 
-Vector3f Fragment::computeColorPhong(std::vector<Light>)
+Vector3f Fragment::computeColorPhong(std::vector<Light> lights)
 {
-	return Vector3f();
+	Vector3f color;
+
+	for (auto& light : lights)
+	{
+		// TODO:
+	}
+
+	return color;
 }
